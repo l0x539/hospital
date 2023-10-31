44df41322950138deb752120fb4e64fa9e52ed09
l0x539 <nogues322@gmail.com>
2023-10-31 06:14:57 +0100

    [Add] hide controls.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,13 +4,17 @@ import store from "@/store"
 import { FC, ReactNode } from "react"
 import { Provider } from "react-redux"
 import MainCanva from "../MainCanva"
+import { Leva } from "leva"
+import { useSearchParams } from "next/navigation"
 
 const Layout: FC<{
   children: ReactNode
 }> = ({children}) => {
+  const searchParams = useSearchParams();
   return <Provider store={store}>
     <main className="flex h-screen min-w-[100dvw]">
       {children}
+      <Leva collapsed={false} hidden={!searchParams.has('controls')} />
       <MainCanva />
     </main>
   </Provider>
