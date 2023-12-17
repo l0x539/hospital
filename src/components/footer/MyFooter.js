e2bb0629aefe0ec2a2278a81cf043738669bd447
H1cko <hichemcikouk@gmail.com>
2023-12-17 21:17:52 +0100

    footer is responsive

--- src/components/footer/MyFooter.js
+++ src/components/footer/MyFooter.js
@@ -4,27 +4,27 @@ import './MyFooter.css';
 
 function Footer() {
     return(
-        <footer className="footer items-start flex overflow-hidden text-justify p-[0 20px 40px] mt-[-1px] relative translate-z-0">
-            <div className="footer_container relative pb-[7.5rem] p-10 block max-w-full w-full pb-120\@smallDesk p-40\@smallDesk ">
-                <div className="footer_inner justify-between items-start pt-0 pl-0 pr-0 mt-0 flex flex-row flex-wrap pb-0 max-w-[88.8888888889%] w-[88.8888888889] mt-140\@sm">
+        <footer className="footer items-start flex overflow-hidden text-justify p-[0 20px 40px] mt-[-1px] relative translate-z-0 w-[100vw]">
+            <div className="footer_container pb-[7.5rem] p-[2.5rem] block relative max-w-full w-full">
+                <div className="footer_inner flex justify-between items-start pt-0 pl-0 pr-0 mt-0 max-w-[88.8888888889%] w-[88.8888888889]">
                     <FooterLogo />
-                    <FooterBlock title={"> Navigation"}>
+                    <FooterBlock title={"> Navigation"} className={'block_one'}>
                         <FooterLink href="#" title={"FEATURES +"} />
                         <FooterLink href="#" title={"WHAT CAN YOU BUILD? +"} />
                         <FooterLink href="#" title={"DEVELOPER RESOURCES +"} />
                         <FooterLink href="#" title={"MEDIA +"} />
                         <FooterLink href="#" title={"PARTNERS +"} />
                     </FooterBlock>
-                    <FooterBlock title={"> Developers"}>
+                    <FooterBlock title={"> Developers"} className={'block_two mb-[60px] pt-0 order-1 lg:w-auto shrink-0 w-1/2'}>
                         <FooterLink href="#" title={"DOCS +"} />  
                         <FooterLink href="#" title={"CODE +"} /> 
                     </FooterBlock>
-                    <FooterBlock title={"> Network"}>
+                    <FooterBlock title={"> Network"} className={'block_three shrink-0 mb-[60px] pl-0 pt-0 order-1 w-auto'}>
                         <FooterLink href="#" title={"BLOCK EXPLORER +"} /> 
                         <FooterLink href="#" title={"NETWORK STATUS PAGE +"} />
                         <FooterLink href="#" title={"FAUCET +"} />   
                     </FooterBlock>
-                    <FooterBlock title={"> Learn"}>
+                    <FooterBlock title={"> Learn"} className={'block_four pl-0 shrink-0 order-1 w-auto'}>
                         <FooterLink href="#" title={"CAREERS +"} />
                         <FooterLink href="#" title={"DISCLAIMERS +"} />
                         <FooterLink href="#" title={"BLOG +"} />
@@ -32,9 +32,9 @@ function Footer() {
                         <FooterLink href="#" title={"T&C'S +"} />
                     </FooterBlock>
                 </div>
-                <div className="absolute justify-between items-center flex bottom-10 flex-row left-0 w-full footer_bottom justify-between\@sm">
+                <div className="footer_bottom  absolute justify-between items-center flex bottom-10 flex-row left-0 w-full justify-between\@sm">
                     
-                    <div className="justify-end flex pl-5 right-[45px] order-1 w-auto shrink-0 bg-black z-10 footer_block_social">
+                    <div className="footer_blockSocial justify-end flex pl-5 right-[45px] order-1 w-auto shrink-0 bg-black z-10 footer_block_social">
                      <FooterBlockSocial title={"CONNECT WITH US:"}>
                         <FooterIconLink href="#" svgSrc={"/twitter.svg#twitter"}/>
                         <FooterIconLink href="#" svgSrc={"/discord.svg#discord"}/>
@@ -44,10 +44,10 @@ function Footer() {
                         <FooterIconLink href="#" svgSrc={"/tiktok.svg#tiktok"}/>
                     </FooterBlockSocial>
                     </div>
-                    <div className="text-[.875rem] leading-[18px] pr-5 block bg-black z-10 ">
+                    <div className="legal text-[.875rem] leading-[18px] pr-5 block bg-black z-10 ">
                         <p className="text-[--icon-color] mb-0 mt-0 text-[.875] leading-[18px]">Dominant Strategies</p>
                     </div>
-                    <div className="left-2/4 pl-5 text-[.875rem] pr-5 translate-x-[-50%] absolute block bg-black z-10 ">
+                    <div className="year left-2/4 pl-5 text-[.875rem] pr-5 translate-x-[-50%] absolute block bg-black z-10 ">
                         <p className="text-[--icon-color] mb-0 mt-0">© 2023</p>
                     </div>
                     
@@ -63,7 +63,11 @@ export default Footer;
 
 const FooterLogo = () => {
     return <>
-    <a className="left-0 top-0 w-auto relative FooterLogo relative\@smallDesk"></a>
+    <a className="FooterLogo flex left-0 top-0 w-auto transform-none shrink-0 relative">
+    <svg aria-hidden="true" class="header__logo__circle ">
+<use xlinkHref="/images.svg"></use>
+</svg>
+    </a>
     </>
 };
 
@@ -73,8 +77,8 @@ const FooterBlock = ({
     className
 }) => {
 
-    return <div className="footer_block">
-        <span className="text-white text-lg">{title}</span>
+    return <div className={` ${className}`}>
+        <span className="text-white text-lg footer_span">{title}</span>
         <ul className="mt-[20px] mb-0 ml-0 p-0">
             {children}
         </ul>
@@ -103,7 +107,7 @@ return <>
 
     const FooterIconLink = ({svgSrc, href}) => {
 
-        return <li className="mx-[12px] my-0 w-[22px] fill--icon-color font-normal not-italic">
+        return <li className="mx-[12px] my-0 w-[22px] fill--icon-color font-normal not-italic ">
             <a href={href} className="whitespace-nowrap	">
                 <svg className="h-[1.1875rem] w-[1.1875rem] max-w-full">
                     <use xlinkHref={svgSrc}></use>
